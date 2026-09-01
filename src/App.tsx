4aca4e27afd2f8ff77864c8a4c89055d9237c717
João Guilherme Amancio <joaoguiam@gmail.com>
2026-09-01 15:53:30 -0300

    feat: ativa Lenis (smooth scroll) no App + CSS de suporte

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { AppProvider } from './context/AppContext';
+import { useLenis } from './hooks/useLenis';
 import { Nav } from './components/Nav';
 import { Hero } from './components/Hero';
 
 export default function App() {
+  useLenis();
+
   return (
     <AppProvider>
       <Nav />
