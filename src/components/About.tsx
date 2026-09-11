ccfae555bfb793068e20859a0d6595f8846bf75f
João Guilherme Amancio <joaoguiam@gmail.com>
2026-09-11 09:53:13 -0300

    revert: volta o avatar chibi flutuante no About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -26,7 +26,6 @@ export function About() {
   const bioRef = useReveal<HTMLDivElement>();
   const spotifyRef = useReveal<HTMLDivElement>();
   const tiltRef = useTilt<HTMLDivElement>(5);
-  const photoRef = useTilt<HTMLImageElement>(4);
 
   const { t } = useApp();
   const [featured, ...rest] = TRACKS;
@@ -51,7 +50,7 @@ export function About() {
             </div>
           </div>
           <div className="about-avatar-wrap">
-            <img className="about-avatar" src="/foto.png" alt="João Guilherme" ref={photoRef} />
+            <img className="about-avatar" src="/avatar.png" alt="João Guilherme" />
           </div>
         </div>
 
